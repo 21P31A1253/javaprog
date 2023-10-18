4ac1464ad8d37af228ed61f709866759796c3a06
shiva kumar <99863955+21P31A1253@users.noreply.github.com>
2023-10-18 10:19:34 +0530

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,12 @@
+class Cast2
+{
+ public static void main(String args[])
+  {
+    int x=4,n=5,sum=2;
+
+    for(int i=1;i<=n;i++)
+       sum=sum+(int)Math.pow(x,i);
+
+ System.out.println(sum);
+}
+}
